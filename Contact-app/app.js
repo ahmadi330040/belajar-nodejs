23dd6ad41b3756a852e955e982e00c4fecaee2c1
ahmadi330040 <ahmadi330040@gmail.com>
2021-07-26 21:18:32 +0700

    Membuat Contact App

---
+++ Contact-app/app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const port = 3000
+
+app.set('view engine', 'ejs') //gunakan ejs
+app.use(expressLayouts) //middleware third party
+app.use(express.static('public')) //Builtin middleware
+
+app.get('/', (req, res) => {
+	const mahasiswa = [
+		{
+			nama: 'Ahmadi',
+			email: 'ahmadi@gmail.com',
+		},
+		{
+			nama: 'dony',
+			email: 'donny@gmail.com',
+		},
+		{
+			nama: 'rendi',
+			email: 'rendi@gmail.com',
+		},
+	]
+	res.render('index', {
+		nama: 'Ahmadi',
+		title: 'Halaman Home',
+		mahasiswa,
+		layout: 'layouts/main-layout',
+	})
+})
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman About',
+	})
+})
+
+app.get('/contact', (req, res) => {
+	res.render('contact', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman Contact',
+	})
+})
+
+app.use('/', (req, res) => {
+	res.status(404)
+	res.send('<h1>404</h1>')
+})
+
+app.listen(port, () => {
+	console.log(`listening at http://localhost:${port}`)
+})
--- express-middleware/app.js
+++ express-middleware/app.js
@@ -11,15 +11,15 @@ app.set('view engine', 'ejs')
 app.use(morgan('dev'))
 app.use(expressLayouts)
 
-//Builtin middleware 
+//Builtin middleware
 app.use(express.static('public'))
 
 // Appliication level middleware
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	console.log('Time :', Date.now())
 	next()
 })
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	console.log('middleware ke 2')
 	next()
 })
@@ -39,23 +39,26 @@ app.get('/', (req, res) => {
 			email: 'rendi@gmail.com',
 		},
 	]
-	res.render('index', { nama: 'Ahmadi',
-	 title: 'Halaman Home', 
-	 mahasiswa
-		, layout: 'layouts/main-layout' })
+	res.render('index', {
+		nama: 'Ahmadi',
+		title: 'Halaman Home',
+		mahasiswa,
+		layout: 'layouts/main-layout',
+	})
 })
 
 app.get('/about', (req, res) => {
 	res.render('about', {
-	layout : 'layouts/main-layout'
-	,title : 'Halaman About'
+		layout: 'layouts/main-layout',
+		title: 'Halaman About',
 	})
 })
 
 app.get('/contact', (req, res) => {
 	res.render('contact', {
 		layout: 'layouts/main-layout',
-		title : 'Halaman Contact'})
+		title: 'Halaman Contact',
+	})
 })
 
 app.get('/product/:id', (req, res) => {
