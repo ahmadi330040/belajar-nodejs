367520f2ae886ad0b269bb6d1d9b81bac47999ce
ahmadi330040 <ahmadi330040@gmail.com>
2021-07-31 16:15:44 +0700

    penambahan method override

--- mongo-contact-app/app.js
+++ mongo-contact-app/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 
 const { body, validationResult, check } = require('express-validator')
+const methodOverride = require('method-override')
 
 const session = require('express-session')
 const cookieParser = require('cookie-parser')
@@ -17,6 +18,9 @@ const port = 3000
 app.listen(port, () => {
 	console.log(`mongo contact app | listen on posrt http://localhost:${port}`)
 })
+
+// Setup Override
+app.use(methodOverride('_method'))
 
 // Setup EJS
 app.set('view engine', 'ejs') //gunakan ejs
@@ -123,17 +127,34 @@ app.post(
 )
 
 // Proses Hapus data contact
-app.get('/contact/delete/:nama', async (req, res) => {
-	const contact = await Contact.findOne({ nama: req.params.nama })
-	// Jika contact tidak ada tampilkan 404
-	if (!contact) {
-		res.status(404), res.send('<h1>404</h1>')
-	} else {
-		Contact.deleteOne({ _id: contact._id }).then((result) => {
-			req.flash('msg', 'Data berhasil dihapus')
-			res.redirect('/contact')
-		})
-	}
+// app.get('/contact/delete/:nama', async (req, res) => {
+// 	const contact = await Contact.findOne({ nama: req.params.nama })
+// 	// Jika contact tidak ada tampilkan 404
+// 	if (!contact) {
+// 		res.status(404), res.send('<h1>404</h1>')
+// 	} else {
+// 		Contact.deleteOne({ _id: contact._id }).then((result) => {
+// 			req.flash('msg', 'Data berhasil dihapus')
+// 			res.redirect('/contact')
+// 		})
+// 	}
+// })
+
+app.delete('/contact', (req, res) => {
+	Contact.deleteOne({ nama: req.body.nama }).then((result) => {
+		req.flash('msg', 'Data berhasil dihapus')
+		res.redirect('/contact')
+	})
+})
+
+// HalamaUubah contact berdasarkan Nama
+app.get('/contact/edit/:nama', async (req, res) => {
+	const contact = await Contact.findOne(req.params.nama)
+	res.render('edit-contact', {
+		layout: 'layouts/main-layout',
+		title: 'Halaman Edit Contact',
+		contact,
+	})
 })
 
 // Halaman Detail
